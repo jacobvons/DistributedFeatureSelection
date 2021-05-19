d3efcb324ac9ef5fe501c5b98dfaab32ad4349f1
jacobvons <jacobhwt0018@outlook.com>
2021-05-19 15:29:28 +1000

    all files

---
+++ conv_iter.py
@@ -0,0 +1,34 @@
+"""
+This file contains core functions for the iterative converge process
+"""
+
+
+def get_top_h(feature_dicts: list, h: int) -> list:
+    """
+    From all features from all nodes, choose top h features according to their importance value
+    and output the features
+    :param feature_dicts: list, contains feature dicts from all nodes with feature name being the key and
+    feature "importance value" being the value
+    :param h: int, number of features we want to select
+    :return: list, a list of features with highest importance value among all
+    """
+    all_features_dict = {}
+    for fd in feature_dicts:
+        for f_name, value in fd.items():
+            if f_name in all_features_dict.keys():
+                all_features_dict[f_name] += value
+            else:
+                all_features_dict[f_name] = value
+
+    all_features_list = [k for k, v in sorted(all_features_dict.items(), key=lambda item: item[1], reverse=True)]
+    return all_features_list[0:h]
+
+
+def node_converged(node_fs: list, new_fs: list) -> bool:
+    """
+    To check if a node has got all the features in the newly generated feature set
+    :param node_fs: list, existing features on a node
+    :param new_fs: list, newly aggregated features by all nodes
+    :return: bool, True if a node has all features in the new feature list, otherwise False
+    """
+    return len(node_fs) == len(set(node_fs + new_fs))
---
+++ voting.py
@@ -0,0 +1,38 @@
+"""
+The below codes construct a function which aggregate the features using a voting method
+In data partition step, we will partition the data into n subsets and evaluate each subset via some FS algorithm.
+Suppose we use CIFE from skfeature, it will return a list of m features for a given data set using CIFE.
+We combine the ranking for each subset by voting
+"""
+import numpy as np
+
+def voting(feature_ranking, total_n):
+    """
+    Inputs:
+    - An n*m array which contains the ranking for all subsets
+    where m is the number of the feature we want as the result
+    n is the number of subset from data partition
+    - Total number of feature (i.e The possible feature index)
+    Outputs:
+    - An 1*m array which contains the final feature indexs after voting
+    Assume the index start from zero
+    """
+
+    # Get the number of target features and the number of subsets
+    m = feature_ranking.shape[1]
+    n = feature_ranking.shape[0]
+
+    # Array to store the vote
+    voting_count = np.zeros((1,total_n))
+
+    # Raise the vote for each time one feature appears in the array
+    for i in range(n):
+        for j in range(m):
+
+            # Feature index
+            index = feature_ranking[i,j]
+
+            voting_count[index] += 1
+
+    # Generate the final
+    return voting_count.argsort()[::-1][:m+1]
